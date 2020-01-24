c9fec57ed35f7e42983712c47b3174f01eb17c9f
kingzmen <kingzmentechnology@gmail.com>
2020-01-24 05:50:44 +0100

    first commit

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://kingztech:winnersme@cluster0-ocddg.gcp.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true })
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const { User } = require('./models/user')
+const config = require('./config/key')
+
+mongoose.connect(config.mongoURI, { useNewUrlParser: true })
     .then(() => console.log('DB connected'))
     .catch(err => console.error(err))
 
 
 
 
-app.get('/', (req, res) => {
-    res.send('hello world')
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post('/api/users/register', (req, res) => {
+    const user = new User(req.body)
+    user.save((err, userData) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+
 })
+
+
+
 app.listen(5000);
